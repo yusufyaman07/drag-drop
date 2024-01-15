cd957646d7781759650fcb7bd233b09e831c03ab
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-01-15 13:40:11 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+/* //!CONTROLE
+console.log("Hi Js,I am Yusuf YAMAN")
+*/
+let lists = document.getElementsByClassName("list");
+let leftBox = document.getElementById("left-box");
+let rightBox = document.getElementById("right-box");
+
+for (lists of lists) {
+  lists.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    rightBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    rightBox.addEventListener("drop", function (e) {
+      rightBox.appendChild(selected);
+      selected=null;
+    });
+    leftBox.addEventListener("dragover", function (e) {
+        e.preventDefault();
+      });
+      leftBox.addEventListener("drop", function (e) {
+        leftBox.appendChild(selected);
+        selected=null;
+      });
+  });
+}
